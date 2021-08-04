53794e818f25bf478d02f7666f6b023dbc749bde
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-08-04 07:52:40 +0100

    Creating main sections (pages), adding external fonts, creating landing page, splitting the page into components, adding static assets, adding header component, creating an admin section, preparing backend, adding separate layout to the admin area and error

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,12 +9,13 @@ module.exports = {
     head: {
         title: pkg.name,
         meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: pkg.description }
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description }
         ],
         link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap' }
         ]
     },
 
