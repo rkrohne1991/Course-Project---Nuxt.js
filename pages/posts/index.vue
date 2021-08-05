6c466befbb4cd03ca88e67118053c0a12fcfc727
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-08-05 08:39:02 +0100

    Adding the VuexStore, Vuex, fetch() and nuxtServerInit()

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -12,25 +12,37 @@ export default {
     components: {
         PostList
     },
-    asyncData(context) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve({
-                    loadedPosts: [
-                        { id: '1', title: 'First Post', previewText: 'This is our first post!', thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg' },
-                        { id: '2', title: 'Second Post', previewText: 'This is our second post!', thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg' }
-                    ]
-                });
-            }, 1500);
-            // reject(new Error());
-        })
-        .then(data => {
-            return data;
-        })
-        .catch(e => {
-            context.error(e);
-        });
-    },
+    // fetch(context) {
+        // if (context.store.state.loadedPosts.length > 0) {
+        //     return null;
+        // }
+        // return new Promise((resolve, reject) => {
+        //     setTimeout(() => {
+        //         resolve({
+        //             loadedPosts: [
+        //                 { id: '1', title: 'First Post', previewText: 'This is our first post!', thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg' },
+        //                 { id: '2', title: 'Second Post', previewText: 'This is our second post!', thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg' }
+        //             ]
+        //         });
+        //     }, 1500);
+        //     // reject(new Error());
+        // })
+        // .then(data => {
+        //     // return data;
+        //     context.store.commit('setPosts', data.loadedPosts);
+        // })
+        // .catch(e => {
+        //     context.error(e);
+        // });
+    // },
+    computed: {
+        loadedPosts() {
+            return this.$store.getters.loadedPosts;
+        }
+    }
+    // created() {
+    //     this.$store.dispatch('setPosts', this.loadedPosts);
+    // }
 }
 
 </script>
